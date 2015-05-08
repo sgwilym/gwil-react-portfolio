9ecb3f231c05bce845f868441ed36e239219a234
Samuel Gwilym <sam@comique.co>
2015-05-08 15:52:43 +0200

    Remove react-a11y

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Page from './components/Page';
 import PageData from './PageData'
-import a11y from 'react-a11y';
 require('../public/style/gwilco.scss');
 
 React.render(
